3a02404973d6327875753c682a4d5e9be98db11c
Jared Jenvey <jared@exit-12.com>
2023-02-07 05:17:27 +0000

    App now stores data in local storage before page unloads, and pulls data from local storage upon return. #1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,14 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousDataJSON = localStorage.getItem('entryStorage');
+
+window.addEventListener('beforeunload', function (e) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('entryStorage', dataJSON);
+});
+
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
