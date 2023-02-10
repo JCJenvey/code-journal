42c10bbd3f0eebe7ddca0aa159f0e33e6e691e3e
Jared Jenvey <jared@exit-12.com>
2023-02-10 04:13:21 +0000

    Finished creation and appending of the DOM entries #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var $photo = document.querySelector('#photo');
 var $journalPhoto = document.querySelector('.journal-photo');
 var $newEntry = document.querySelector('.new-entry');
+var $entryList = document.querySelector('.entry-list');
 
 $photo.addEventListener('input', function (e) {
   $journalPhoto.setAttribute('src', e.target.value);
@@ -19,3 +20,45 @@ $newEntry.addEventListener('submit', function (e) {
   $journalPhoto.setAttribute('src', 'images/placeholder-image-square.jpg');
   $newEntry.reset();
 });
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var $entry = renderEntry(data.entries[i]);
+    $entryList.appendChild($entry);
+  }
+});
+
+function renderEntry(entry) {
+  var $entry = document.createElement('li');
+
+  var $row = document.createElement('div');
+  $row.setAttribute('class', 'row');
+
+  var $firstColumn = makeColumnHalf();
+
+  var $img = document.createElement('img');
+  $img.setAttribute('src', entry.photoUrl);
+  $img.setAttribute('alt', 'The image for a journal entry');
+  $img.setAttribute('onerror', 'src=\'images / placeholder - image - square.jpg\';');
+
+  var $secondColumn = makeColumnHalf();
+
+  var $title = document.createElement('h3');
+  $title.textContent = entry.title;
+
+  var $notes = document.createElement('p');
+  $notes.textContent = entry.notes;
+
+  $entry.append($row);
+  $row.append($firstColumn, $secondColumn);
+  $firstColumn.append($img);
+  $secondColumn.append($title, $notes);
+
+  return $entry;
+}
+
+function makeColumnHalf() {
+  var col = document.createElement('div');
+  col.setAttribute('class', 'column-half');
+  return col;
+}
